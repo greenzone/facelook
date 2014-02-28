72c5b7c14e0ed48269444ee9a1d15de5debdc938
Greenbox.id <info@gb.co.id>
2014-02-28 16:58:00 +0700

    Update 404.php

--- 404.php
+++ 404.php
@@ -6,11 +6,12 @@
 		</header>
 		<div><img itemprop="logo" src="<?php echo get_template_directory_uri();?>/img/404.jpg" style="text-align:center;"></div>
 		<div>
-		    <p style="font-size:17px;"><?php _e(
-                'Tampaknya kami tidak dapat menemukan apa yang Anda cari.<br/> Mungkin halaman sudah kami hapus, atau salah penulisan pada link alamat halaman.<br/> Mohon maaf atas ketidakyamanan ini.',
-                'albert_menangis'
-            ); ?></p>
-		</div><!-- .row content -->
+			 <p style="font-size:17px;"><?php _e(
+                	'Tampaknya kami tidak dapat menemukan apa yang Anda cari.<br/> Mungkin halaman sudah kami hapus, atau salah penulisan pada link alamat halaman.<br/> Mohon maaf atas ketidakyamanan ini.',
+                	'albert_menangis'
+            		); ?>
+            		</p>
+		</div>
 	</div>
 </div>
 <?php get_footer(); ?>
