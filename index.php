2564ba98d665ce84ac4a9d0b17a7e9f33acb511b
Greenbox.id <info@gb.co.id>
2014-02-28 22:22:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,11 @@
 				<div class="meta-post">
 					<i class="fa fa-hand-o-right"></i>&nbsp;Diterbitkan pada:&nbsp;<i class="fa fa-clock-o"></i>&nbsp;<?php the_time('j F Y'); echo ' at '; the_time('H:i'); ?>
 				</div>
+				<div class="thumbnail-single-post">
+					<div class="single-post-tumb">
+						<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_post_thumbnail( array(100,100) ); ?></a>
+					</div>
+				</div>
 				<?php echo get_excerpt(555); ?>
 				<div class="meta">
 					<i class="fa fa-paperclip"></i>&nbsp;Categories:&nbsp;<?php the_category(' - '); ?>
@@ -22,8 +27,8 @@
 		<?php if(get_next_posts_link() != "" && is_paged()) { ?><input type="hidden" id="nextpage" value="<?php next_posts(); ?>" /><?php } ?>
 	</div>
 	<div class="navigation">
-		<div class="next"><i class="fa fa-hand-o-down"></i> <?php next_posts_link('Show Older Posts..') ?></div>
-		<div class="prev"><i class="fa fa-hand-o-up"></i> <?php previous_posts_link('Show Newer Posts..') ?></div>
+		<div class="next"><i class="fa fa-hand-o-down"></i>&nbsp;<?php next_posts_link('Show Older Posts..') ?></div>
+		<div class="prev"><i class="fa fa-hand-o-up"></i>&nbsp;<?php previous_posts_link('Show Newer Posts..') ?></div>
 	</div>
 	<input type="hidden" name="title" value="<?php wp_title( '-', true, 'right' ); echo wp_specialchars( get_bloginfo('name'), 1 ) ?>" />
 	</div>
