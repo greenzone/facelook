e865e7562abb05bdf81d3094f1d32b2fd0f0853b
Greenbox.id <info@gb.co.id>
2014-02-28 19:38:43 +0700

    Update page.php

--- page.php
+++ page.php
@@ -2,27 +2,22 @@
 
 <div id="post-container">
 	<div id="posts">
-
-<?php while(have_posts()) : the_post(); ?>
-	<div class="post">
-		<div class="post-gravatar">
-			<a href="<?php echo get_author_posts_url($authordata->ID); ?>"><?php echo get_avatar(get_the_author_email(), '50') ?></a>
-		</div>
-		<div class="post-text">
-			<h2 class="title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<?php the_content('(more..)'); ?>
-			<div class="post-meta">
-				<?php the_time('j F Y'); echo ' at '; the_time('H:i'); ?> - 
-				<a class="respondlink notajax" href="<?php the_permalink(); ?>#respond">Comments</a> - 
-				<?php edit_post_link('Edit', ' - ', '') ?>
+		<?php while(have_posts()) : the_post(); ?>
+			<div class="post">
+				<div class="post-text">
+					<div class="title-post">
+						<div class="icon-title"><img src="<?php echo get_template_directory_uri();?>/images/judul.svg"></div>
+						<h2 class="title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+					</div>
+					<?php the_content('(more..)'); ?>
+					<div class="post-meta">
+						<i class="fa fa-clock-o"></i>&nbsp;<?php the_time('j F Y'); echo ' at '; the_time('H:i'); ?>
+					</div>
+					<?php comments_template(); ?>
+				</div>
 			</div>
-			<?php comments_template(); ?>
-		</div>
-	</div>
-<?php endwhile; ?>
-
+		<?php endwhile; ?>
 	<input type="hidden" name="title" value="<?php wp_title( '-', true, 'right' ); echo wp_specialchars( get_bloginfo('name'), 1 ) ?>" />
-
 	</div>
 </div>
 	
